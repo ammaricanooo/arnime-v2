0d88ab41c01458ebf85dea119ad10bc70db4c93f
ammaricanooo <ammarabdul27@gmail.com>
2026-03-22 20:00:56 +0700

    edit: origin & referer

--- app/api/getiframe/route.ts
+++ app/api/getiframe/route.ts
@@ -17,7 +17,7 @@ export async function GET(req: Request) {
         }
 
         const BASE_URL = "https://cors.eu.org/https://otakudesu.blog";
-        
+
         // 1. Decode content dari Base64
         let decodedContent: string;
         try {
@@ -45,19 +45,19 @@ export async function GET(req: Request) {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-      "accept-language": "id-ID",
-      "cache-control": "no-cache",
-      pragma: "no-cache",
-      "sec-ch-ua": '"Chromium";v="127", "Not)A;Brand";v="99"',
-      "sec-ch-ua-mobile": "?1",
-      "sec-ch-ua-platform": '"Android"',
-      "sec-fetch-dest": "document",
-      "sec-fetch-mode": "navigate",
-      "sec-fetch-site": "same-origin",
-      "upgrade-insecure-requests": "1",
-      "user-agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36"
-                'Origin': BASE_URL,
-                'Referer': BASE_URL,
+                "accept-language": "id-ID",
+                "cache-control": "no-cache",
+                pragma: "no-cache",
+                "sec-ch-ua": '"Chromium";v="127", "Not)A;Brand";v="99"',
+                "sec-ch-ua-mobile": "?1",
+                "sec-ch-ua-platform": '"Android"',
+                "sec-fetch-dest": "document",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "same-origin",
+                "upgrade-insecure-requests": "1",
+                "user-agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36",
+                'Origin': 'https://otakudesu.blog',
+                'Referer': 'https://otakudesu.blog/',
                 'X-Requested-With': 'XMLHttpRequest'
             }
         });
